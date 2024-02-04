606ca8183c27c2103583d88b4cef5f3aa5a15583
Paolo Bernardi <paolo@bernardi.cloud>
2024-02-04 23:14:53 +0100

    Multiple feature selection strategies, hyperparam opt for all models, metrics other than accuracy

--- check.py
+++ check.py
@@ -49,6 +49,31 @@ def days_to_domain_expire(domain):
     return days_to_expire
 
 
+def check_favicon(soup, netloc):
+    links = soup.findAll('link')
+    if not links:
+        return -1
+    mismatch = 0
+    for link in links:
+        if 'rel' in link and 'href' in link:
+            if link['rel'] == 'icon':
+                src = urlparse(link.href['href'])
+                if src.netloc != netloc:
+                    mismatch += 1
+    return mismatch > 0 and 1 or - 1
+
+
+def check_iframe(soup):
+    iframes = soup.findAll('iframe')
+    if not iframes:
+        return -1
+    mismatch = 0
+    for iframe in iframes:
+        if 'frameborder' in iframe:
+            mismatch += 1
+    return mismatch > 0 and 1 or -1
+
+
 def check_port(parsed_url):
     colon = parsed_url.netloc.split(':')
     if len(colon) == 1:
@@ -63,6 +88,8 @@ def check_port(parsed_url):
 
 def check_url_request(soup, netloc):
     imgs = soup.findAll('img')
+    if not imgs:
+        return -1
     mismatch = 0
     tot = 0
     for img in imgs:
@@ -84,6 +111,8 @@ def check_url_request(soup, netloc):
 
 def check_url_anchors(soup, netloc):
     anchors = soup.findAll('a')
+    if not anchors:
+        return -1
     mismatch = 0
     tot = 0
     for anchor in anchors:
@@ -102,23 +131,26 @@ def check_url_anchors(soup, netloc):
     else:
         return -1
 
+
 def check_link_in_tags(soup, netloc):
     mismatch = 0
     tot = 0
     scripts = soup.findAll('script')
-    for script in scripts:
-        if 'src' in script:
-            src = urlparse(scripts['src'])
-            tot += 1
-            if src.netloc != netloc:
-                mismatch += 1
+    if scripts:
+        for script in scripts:
+            if 'src' in script:
+                src = urlparse(scripts['src'])
+                tot += 1
+                if src.netloc != netloc:
+                    mismatch += 1
     links = soup.findAll('link')
-    for link in links:
-        if 'href' in link:
-            href = urlparse(link['href'])
-            tot += 1
-            if href.netloc != netloc:
-                mismatch += 1
+    if links:
+        for link in links:
+            if 'href' in link:
+                href = urlparse(link['href'])
+                tot += 1
+                if href.netloc != netloc:
+                    mismatch += 1
     if tot == 0:
         return 1
     perc = mismatch * 100 / tot
@@ -129,8 +161,11 @@ def check_link_in_tags(soup, netloc):
     else:
         return -1
 
+
 def check_form_handlers(soup, netloc):
     forms = soup.findAll('form')
+    if not forms:
+        return -1
     for form in forms:
         if 'action' in form:
             action = urlparse(form['action'])
@@ -138,13 +173,17 @@ def check_form_handlers(soup, netloc):
                 return 1
     return -1
 
+
 def check_submit_email(soup):
     forms = soup.findAll('form')
+    if not forms:
+        return -1
     for form in forms:
         if 'action' in form:
             if 'mailto' in form['action']:
                 return 1
     return -1
+
 
 def check_abnormal_url(domain):
     w = whois.whois(domain)
@@ -155,6 +194,7 @@ def check_abnormal_url(domain):
     else:
         return 1
 
+
 def check_redirects(url):
     session = requests.Session()
     session.max_redirects = 1
@@ -164,17 +204,20 @@ def check_redirects(url):
     except requests.exceptions.TooManyRedirects as exc:
         return 1
 
+
 def check_onmouseover(html):
     if 'onmouseover' in html and 'window.status' in html:
         return 1
     else:
         return -1
 
+
 def check_rightclick(html):
     if 'event.button==2' in html:
         return 1
     else:
         return -1
+
 
 def check_domain_age(domain):
     now = datetime.now()
@@ -187,12 +230,14 @@ def check_domain_age(domain):
     days_from_creation = timedelta.days
     return days_from_creation > 180 and -1 or 1
 
+
 def check_dnsrecord(domain):
     try:
         socket.getaddrinfo(domain, None, socket.AF_UNSPEC)
         return -1
     except:
         return 1
+
 
 def check_traffic(domain):
     try:
@@ -206,6 +251,7 @@ def check_traffic(domain):
             return 1
     except:
         return -1
+
 
 def get_ranks(domain):
     headers = {
@@ -232,12 +278,14 @@ def get_ranks(domain):
     ranks.append(int(re.findall(r"External Backlinks: (\d+)", res)[0].replace(',', '')))
     return ranks
 
+
 def check_statistical_report(domain):
     with open("phishing-domains.txt") as f:
         for line in f.readlines():
             if domain == line.strip():
                 return 1
     return -1
+
 
 def get_url_features(url):
     f = {}
@@ -269,10 +317,13 @@ def get_url_features(url):
     ctx.verify_mode = ssl.CERT_NONE
     html = requests.get(url, verify=False).text
     soup = BeautifulSoup(html, 'html.parser')
+    f['Favicon'] = check_favicon(soup, parsed_url.netloc)
     f['Request URL'] = check_url_request(soup, parsed_url.netloc)
     f['URL of Anchor'] = check_url_anchors(soup, parsed_url.netloc)
     f['Links in Tags'] = check_link_in_tags(soup, parsed_url.netloc)
     f['SFH'] = check_form_handlers(soup, parsed_url.netloc)
+    f['PopUp Window'] = check_form_handlers(soup, parsed_url.netloc)
+    f['IFrame'] = check_iframe(soup)
     f['Submitting to Email'] = check_submit_email(soup)
     print(colored('[ 30%]', 'magenta'), 'WHOIS...')
     f['Abnormal URL'] = check_abnormal_url(host)
@@ -302,19 +353,18 @@ def get_url_features(url):
         f['Links Pointing to Page'] = 0  # No information = cannot draw conclusions...
     print(colored('[ 90%]', 'magenta'), 'Statistical report...')
     f['Statistical Report'] = check_statistical_report(host)
+    print("\n".join(f.keys()))
     print(colored('[100%]', 'magenta'), 'Done.')
     return f
+
 
 def print_features(features):
     for k, v in features.items():
         print(f"  {k} =", COLORED[v])
-        
+
 
 if __name__ == '__main__':
     # See https://openphish.com/ for live phishing examples
-    #website = 'https://expired.badssl.com/'
-    #website = 'https://marketmonitor-qa-allegrocz.istest.pl/login'
-    #website = 'https://webmail-109686.weeblysite.com/'
     #website = 'http://ploshadka.top/irsus/payment.html'
     print()
     cprint(' --- PHISHING WEBSITE CHECKER --- ', 'white', 'on_cyan')
@@ -325,9 +375,12 @@ if __name__ == '__main__':
     f = get_url_features(website)
     print()
     print_features(f)
+    fselect = joblib.load('fselect.joblib')
     clf = joblib.load('rf.joblib')
     print(colored('\nLoading classifier...', 'cyan'))
-    y = clf.predict([[float(v) for v in f.values()]])
+    X = [[float(v) for v in f.values()]]
+    X_new = fselect.transform(X)
+    y = clf.predict(X_new)
     if y[0] == 1:
         print(colored('ACHTUNG: POSSIBLE PHISHING ATTEMPT', 'red'))
     else:
